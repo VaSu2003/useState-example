1d2fc0625ab2054015417b8857fcf4979bda4da5
Vaibhav <vaibhav.shukla98871174@gmail.com>
2023-07-25 10:30:04 +0530

    use useReducer to handle the states

--- src/useReducer.jsx
+++ src/useReducer.jsx
@@ -1,21 +1,47 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import {data} from './data.js'
+
+const defaultState = {
+    people:data,
+}
+
+const reducer = (state,action)=>{
+    if(action.type === "CLEAR_LIST"){
+        return {...state,people:[]}
+    }else if(action.type === "RESET_LIST"){
+        return {...state,people:data}
+    }else if(action.type === "DELETE"){
+        return {...state,people: state.people.filter((person)=> person.id !== action.id)}
+    }
+    throw new Error(`No matching ${action.type} - action type`)
+}
+
+
   
 function ReducerBasics() {
 
-    const [people, setPeople] = useState(data);
+    const [state,dispatch] = useReducer(reducer,defaultState)
+
+    // const [people, setPeople] = useState(data);
     function deletePerson(id){
-        setPeople(people.filter((person)=> person.id !== id))
+        dispatch({type:"DELETE",id:id})
+        // setPeople(people.filter((person)=> person.id !== id))
     }
     function clearPerson(){
-        setPeople([])
+        dispatch({type:"CLEAR_LIST"})
+        // setPeople([])
     }
+    function resetList(){
+        dispatch({type:"RESET_LIST"})
+        // setPeople(data)
+    }
+    
     return (
         <>
-        {people.length === 0 ? <button onClick={()=>setPeople(data)}>Reset</button> : <>
+        {state.people.length === 0 ? <button onClick={resetList}>Reset</button> : <>
         <ul>
                 {
-                    people.map((person)=>{
+                    state.people.map((person)=>{
                     return (
                         
                         <li key={person.id}>
